a4c4b4b57445d37da6a5d0c79d218501c8fe910e
khakhiD <bfwanso@naver.com>
2022-02-26 20:02:25 +0900

    koongkoongta 01

--- projects/ToyProjects/koongkoongta/src/main.js
+++ projects/ToyProjects/koongkoongta/src/main.js
@@ -1,7 +1,34 @@
 const startButton = document.querySelector(".start_btn");
+const loading = document.querySelector(".start_loading");
+const 
 
 function caution() {
-  alert("현재 개발중입니다.");
+  const nopValue = document.querySelector("#nop_value");
+  const wordLengthValue = document.querySelector("#wordLength_value");
+  const nopValue_int = Number(nopValue);
+  const wordLengthValue_int = Number(wordLengthValue);
+
+  if (nopValue.value == "") {
+    alert("인원 수가 입력되지 않았습니다.");
+    nopValue.focus();
+    return false;
+  } else if (wordLengthValue.value == "") {
+    alert("단어 수가 입력되지 않았습니다.");
+    wordLengthValue.focus();
+    return false;
+  } else if (
+    nopValue_int > 4 ||
+    nopValue_int < 2 ||
+    wordLengthValue_int > 6 ||
+    wordLengthValue_int < 1
+  ) {
+    alert("인원 수는 2~4명, 단어 수는 6글자 이하만 가능합니다.");
+    console.log("제한 테스트");
+    return false;
+  }
+
+  // 아래부터 개발할 것
+
 }
 
 startButton.addEventListener("click", caution);
