b76d26894f255d557ef1fed3dfd4a01f8a138770
khakhiD <bfwanso@naver.com>
2022-03-23 20:07:06 +0900

    dashboard_template-complete

--- projects/ToyProjects/dashboard/EGATOR/index.js
+++ projects/ToyProjects/dashboard/EGATOR/index.js
@@ -16,6 +16,30 @@ closeBtn.addEventListener("click", () => {
 // change theme
 themeToggler.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme-variables");
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+});
 
-  themeToggler.querySelector("span").classList.toggle("active");
+// fill orders in table
+Orders.forEach((order) => {
+  const tr = document.createElement("tr");
+  const trContent = `
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="
+                        ${
+                          order.shipping === "Declined"
+                            ? "danger"
+                            : order.shipping === "Pending"
+                            ? "warning"
+                            : "primary"
+                        }
+                        ">
+                        ${order.shipping}
+                        </td>
+                        <td class="primary">Details</td>
+                        `;
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").appendChild(tr);
 });
