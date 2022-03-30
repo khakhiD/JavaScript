5976ad0da1a30bb3af5d2121bf319e36f948c9bd
khakhiD <bfwanso@naver.com>
2022-03-30 22:54:49 +0900

    solidraw test pages

---
+++ projects/ToyProjects/submit/index.js
@@ -0,0 +1,32 @@
+let submit = document.getElementById("submitButton");
+submit.onclick = showImage; //Submit 버튼 클릭시 이미지 보여주기
+
+function showImage() {
+  let newImage = document.getElementById("image-show").lastElementChild;
+  newImage.style.visibility = "visible";
+
+  document.getElementById("image-upload").style.visibility = "hidden";
+  // document.getElementById("title").style.visibility = "hidden";
+
+  document.getElementById("fileName").textContent = null; //기존 파일 이름 지우기
+}
+
+function loadFile(input) {
+  let file = input.files[0];
+
+  let name = document.getElementById("fileName");
+  name.textContent = file.name;
+
+  let newImage = document.createElement("img");
+  newImage.setAttribute("class", "img");
+
+  newImage.src = URL.createObjectURL(file);
+
+  newImage.style.width = "70%";
+  newImage.style.height = "70%";
+  newImage.style.visibility = "hidden"; //버튼을 누르기 전까지는 이미지 숨기기
+  newImage.style.objectFit = "contain";
+
+  let container = document.getElementById("image-show");
+  container.appendChild(newImage);
+}
