0ef7ecc0ce7795170781f9e81ed6ebde592bfe82
khakhiD <bfwanso@naver.com>
2022-05-11 00:45:03 +0900

    til Contact part

--- projects/ToyProjects/responsive-real-state-website-main/assets/js/main.js
+++ projects/ToyProjects/responsive-real-state-website-main/assets/js/main.js
@@ -6,10 +6,45 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
-
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item')
 
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value__accordion-header')
+    accordionHeader.addEventListener('click', ()=>{
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item);
+
+        if(openItem && openItem!== item){
+            toggleItem(openItem)
+
+        }
+    })
+})
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
