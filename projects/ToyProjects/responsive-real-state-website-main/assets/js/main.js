c95526a48b9cff54513c27bcd071b67eeb927348
khakhiD <bfwanso@naver.com>
2022-05-11 23:04:19 +0900

    solidraw로 변환작업중

--- projects/ToyProjects/responsive-real-state-website-main/assets/js/main.js
+++ projects/ToyProjects/responsive-real-state-website-main/assets/js/main.js
@@ -98,3 +98,22 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`)
+sr.reveal(`.home__description, .footer__info`, {delay: 500})
+sr.reveal(`.home__search`, {delay: 600})
+sr.reveal(`.home__value`, {delay: 700})
+sr.reveal(`.home__images`, {delay: 800, origin: 'bottom'})
+sr.reveal(`.logos__img`, {interval: 100})
+sr.reveal(`.value__images, .contact__content`, {origin: 'left'})
+sr.reveal(`.value__content, .contact__images`, {origin: 'right'})
+
+
+
