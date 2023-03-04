772d3ccf9edbf03a32bfc115aa2d840c9d295301
khakhiD <kakkiid@gmail.com>
2023-03-04 23:49:15 +0900

    refactoring

--- JS_practice/axios-practice/app.js
+++ JS_practice/axios-practice/app.js
@@ -1,6 +1,9 @@
 const form = document.querySelector("#searchForm");
+const imgBox = document.querySelector("#imgBox");
+
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
+  removeChild(imgBox);
   const searchTerm = form.elements.query.value;
   const config = { params: { q: searchTerm } };
   try {
@@ -17,7 +20,16 @@ const makeImages = (name) => {
     if (result.person.image) {
       const img = document.createElement("IMG");
       img.src = result.person.image.medium;
-      document.body.append(img);
+      const box = document.createElement("div")
+      box.setAttribute("id", "imgBox");
+      document.body.append(box);
+      document.querySelector("#imgBox").append(img);
     }
   }
 };
+
+const removeChild = (div) => {
+    while (div.hasChildNodes()) {
+        div.removeChild(div.firstChild);
+    }
+}
